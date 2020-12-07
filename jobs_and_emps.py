65fa1e5eed12e3f261b87e2d0fccfc6ee6105d0b
wcstrickland <73347105+wcstrickland@users.noreply.github.com>
2020-12-06 23:45:01 -0600

    Add files via upload
    created a reset day function so the scheduler function is reusable and added some print statement testing to my random sample testing

--- jobs_and_emps.py
+++ jobs_and_emps.py
@@ -1,4 +1,4 @@
-from scheduler_classes import Job, Employee, MaxHeap
+from scheduler_classes import Job, Employee, MaxHeap, schedule, get_emp_no
 import random
 
 # list of first names for generating random employees
@@ -23,10 +23,10 @@ p5 = MaxHeap(5)
 # generate 100 random jobs
 for i in range(100):
     _ = Job((random.choice(alphabet) + str(random.randint(1, 200))),
-            random.randint(1, 5), random.randint(1, 3), random.randint(1, 5))
+            random.randint(1, 5), random.randint(1, 5), random.randint(1, 5))
 
 # generate 50 random employees
-for i in range(50):
+for i in range(10):
     f_name = random.choice(fnames) + str(random.randint(0, 9))
     l_name = " " + random.choice(lnames) + str(random.randint(0, 9))
     _ = Employee(f_name, l_name)
@@ -44,3 +44,34 @@ if __name__ == '__main__':
     # prints all employees
     for emp in Employee.all_emps:
         print(emp)
+
+    schedule([])
+
+    for heap in MaxHeap.heaps_list:
+        print("*" * 90)
+        print(heap, "\t", "jobs in heap:", len(heap.heap))
+        print("*" * 90)
+        print(heap.heap)
+        print("*" * 90)
+
+    for emp in Employee.all_emps:
+        print(emp)
+
+    emp_0 = get_emp_no(0)
+    print(emp_0.today_jobs)
+
+    schedule([])
+
+    for heap in MaxHeap.heaps_list:
+        print("*" * 90)
+        print(heap, "\t", "jobs in heap:", len(heap.heap))
+        print("*" * 90)
+        print(heap.heap)
+        print("*" * 90)
+
+    for emp in Employee.all_emps:
+        print(emp)
+
+    emp_0 = get_emp_no(0)
+    print(emp_0.today_jobs)
+
